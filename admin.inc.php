5a71cbb7a590c5dc96401c8ede0b8a73a945cabc
Ajay <Ajay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-24 11:38:45 +0000

    git-svn-id: https://plugins.svn.wordpress.org/contextual-related-posts/tags/1.5@186833 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin.inc.php
@@ -0,0 +1,293 @@
+<?php
+/**********************************************************************
+*					Admin Page										*
+*********************************************************************/
+if (!defined('ABSPATH')) die("Aren't you supposed to come here via WP-Admin?");
+
+if (!defined('CRP_LOCAL_NAME')) define('CRP_LOCAL_NAME', 'better-search');
+
+// Pre-2.6 compatibility
+if ( !defined('WP_CONTENT_URL') )
+	define( 'WP_CONTENT_URL', get_option('siteurl') . '/wp-content');
+if ( !defined('WP_CONTENT_DIR') )
+	define( 'WP_CONTENT_DIR', ABSPATH . 'wp-content' );
+// Guess the location
+$crp_path = WP_CONTENT_DIR.'/plugins/'.plugin_basename(dirname(__FILE__));
+$crp_url = WP_CONTENT_URL.'/plugins/'.plugin_basename(dirname(__FILE__));
+
+
+function crp_options() {
+	
+	global $wpdb;
+    $poststable = $wpdb->posts;
+
+	$crp_settings = crp_read_options();
+
+	if($_POST['crp_save']){
+		$crp_settings[title] = ($_POST['title']);
+		$crp_settings[limit] = ((is_int($_POST['limit'])) ? ($_POST['limit']) : 5);
+		$crp_settings[exclude_cat_slugs] = ($_POST['exclude_cat_slugs']);
+		$crp_settings[add_to_content] = (($_POST['add_to_content']) ? true : false);
+		$crp_settings[add_to_page] = (($_POST['add_to_page']) ? true : false);
+		$crp_settings[add_to_feed] = (($_POST['add_to_feed']) ? true : false);
+		$crp_settings[match_content] = (($_POST['match_content']) ? true : false);
+		$crp_settings[exclude_pages] = (($_POST['exclude_pages']) ? true : false);
+		$crp_settings[blank_output] = (($_POST['blank_output'] == 'blank' ) ? true : false);
+		$crp_settings[post_thumb_op] = $_POST['post_thumb_op'];
+		$crp_settings[before_list] = $_POST['before_list'];
+		$crp_settings[after_list] = $_POST['after_list'];
+		$crp_settings[before_list_item] = $_POST['before_list_item'];
+		$crp_settings[after_list_item] = $_POST['after_list_item'];
+		$crp_settings[thumb_meta] = $_POST['thumb_meta'];
+		$crp_settings[thumb_default] = $_POST['thumb_default'];
+		$crp_settings[thumb_height] = ((is_int($_POST['thumb_height'])) ? ($_POST['thumb_height']) : 100);
+		$crp_settings[thumb_width] = ((is_int($_POST['thumb_width'])) ? ($_POST['thumb_width']) : 100);
+		
+		
+		$exclude_categories_slugs = explode(", ",$crp_settings[exclude_cat_slugs]);
+		
+		$exclude_categories = '';
+		foreach ($exclude_categories_slugs as $exclude_categories_slug) {
+			$catObj = get_category_by_slug($exclude_categories_slug);
+			$exclude_categories .= $catObj->term_id . ',';
+		}
+		$crp_settings[exclude_categories] = substr($exclude_categories, 0, -2);
+
+		update_option('ald_crp_settings', $crp_settings);
+		
+		$str = '<div id="message" class="updated fade"><p>'. __('Options saved successfully.',CRP_LOCAL_NAME) .'</p></div>';
+		echo $str;
+	}
+	
+	if ($_POST['crp_default']){
+		delete_option('ald_crp_settings');
+		$crp_settings = crp_default_options();
+		update_option('ald_crp_settings', $crp_settings);
+		
+		$str = '<div id="message" class="updated fade"><p>'. __('Options set to Default.',CRP_LOCAL_NAME) .'</p></div>';
+		echo $str;
+	}
+	if ($_POST['crp_recreate']){
+		$sql = "ALTER TABLE $poststable DROP INDEX crp_related";
+		$wpdb->query($sql);
+		
+		$sql = "ALTER TABLE $poststable DROP INDEX crp_related_title";
+		$wpdb->query($sql);
+		
+		$sql = "ALTER TABLE $poststable DROP INDEX crp_related_content";
+		$wpdb->query($sql);
+		
+		ald_crp_activate();
+		
+		$str = '<div id="message" class="updated fade"><p>'. __('Index recreated',CRP_LOCAL_NAME) .'</p></div>';
+		echo $str;
+	}
+?>
+
+<div class="wrap">
+  <h2>Contextual Related Posts</h2>
+  <div style="border: #ccc 1px solid; padding: 10px">
+    <fieldset class="options">
+    <legend>
+    <h3>
+      <?php _e('Support the Development',CRP_LOCAL_NAME); ?>
+    </h3>
+    </legend>
+    <p>
+      <?php _e('If you find ',CRP_LOCAL_NAME); ?>
+      <a href="http://ajaydsouza.com/wordpress/plugins/contextual-related-posts/">Contextual Related Posts</a>
+      <?php _e('useful, please do',CRP_LOCAL_NAME); ?>
+      <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&amp;business=donate@ajaydsouza.com&amp;item_name=Related%20Posts%20(From%20WP-Admin)&amp;no_shipping=1&amp;return=http://ajaydsouza.com/wordpress/plugins/contextual-related-posts/&amp;cancel_return=http://ajaydsouza.com/wordpress/plugins/contextual-related-posts/&amp;cn=Note%20to%20Author&amp;tax=0&amp;currency_code=USD&amp;bn=PP-DonationsBF&amp;charset=UTF-8" title="Donate via PayPal"><?php _e('drop in your contribution',CRP_LOCAL_NAME); ?></a>.
+	  (<a href="http://ajaydsouza.com/donate/"><?php _e('Some reasons why you should.',CRP_LOCAL_NAME); ?></a>)</p>
+    </fieldset>
+  </div>
+  <form method="post" id="crp_options" name="crp_options" style="border: #ccc 1px solid; padding: 10px" onsubmit="return checkForm()">
+    <fieldset class="options">
+    <legend>
+    <h3>
+      <?php _e('Options:',CRP_LOCAL_NAME); ?>
+    </h3>
+    </legend>
+    <p>
+      <label>
+      <?php _e('Number of related posts to display: ',CRP_LOCAL_NAME); ?>
+      <input type="textbox" name="limit" id="limit" value="<?php echo attribute_escape(stripslashes($crp_settings[limit])); ?>">
+      </label>
+    </p>
+    <p><?php _e('Exclude Categories: ',CRP_LOCAL_NAME); ?></p>
+	<div style="position:relative;text-align:left">
+		<table id="MYCUSTOMFLOATER" class="myCustomFloater" style="position:absolute;top:50px;left:0;background-color:#cecece;display:none;visibility:hidden">
+		<tr><td><!--
+				please see: http://chrisholland.blogspot.com/2004/09/geekstuff-css-display-inline-block.html
+				to explain why i'm using a table here.
+				You could replace the table/tr/td with a DIV, but you'd have to specify it's width and height
+				-->
+			<div class="myCustomFloaterContent">
+			you should never be seeing this
+			</div>
+		</td></tr>
+		</table>
+		<textarea class="wickEnabled:MYCUSTOMFLOATER" cols="50" rows="3" wrap="virtual" name="exclude_cat_slugs"><?php echo (stripslashes($crp_settings[exclude_cat_slugs])); ?></textarea>
+	</div>
+    <p>
+      <label>
+      <input type="checkbox" name="add_to_content" id="add_to_content" <?php if ($crp_settings[add_to_content]) echo 'checked="checked"' ?> />
+      <?php _e('Add related posts to the post content on single posts. <br />If you choose to disable this, please add <code>&lt;?php if(function_exists(\'echo_ald_crp\')) echo_ald_crp(); ?&gt;</code> to your template file where you want it displayed',CRP_LOCAL_NAME); ?>
+      </label>
+    </p>
+    <p>
+      <label>
+      <input type="checkbox" name="add_to_page" id="add_to_page" <?php if ($crp_settings[add_to_page]) echo 'checked="checked"' ?> />
+      <?php _e('Add related posts to pages. <br />If you choose to disable this, please add <code>&lt;?php if(function_exists(\'echo_ald_crp\')) echo_ald_crp(); ?&gt;</code> to your template file where you want it displayed',CRP_LOCAL_NAME); ?>
+      </label>
+    </p>
+    <p>
+      <label>
+      <input type="checkbox" name="add_to_feed" id="add_to_feed" <?php if ($crp_settings[add_to_feed]) echo 'checked="checked"' ?> />
+      <?php _e('Add related posts to feed',CRP_LOCAL_NAME); ?>
+      </label>
+    </p>
+    <p>
+      <label>
+      <input type="checkbox" name="match_content" id="match_content" <?php if ($crp_settings[match_content]) echo 'checked="checked"' ?> />
+      <?php _e('Find related posts based on content as well as title. If unchecked, only posts titles are used. (I recommend using a caching plugin if you enable this)',CRP_LOCAL_NAME); ?>
+      </label>
+    </p>
+    <p>
+      <label>
+      <input type="checkbox" name="exclude_pages" id="exclude_pages" <?php if ($crp_settings[exclude_pages]) echo 'checked="checked"' ?> />
+      <?php _e('Exclude Pages in Related Posts',CRP_LOCAL_NAME); ?>
+      </label>
+    </p>
+    <h4>
+      <?php _e('Output Options:',CRP_LOCAL_NAME); ?>
+    </h4>
+    <p>
+      <label>
+      <?php _e('Title of related posts: ',CRP_LOCAL_NAME); ?>
+      <input type="textbox" name="title" id="title" value="<?php echo attribute_escape(stripslashes($crp_settings[title])); ?>">
+      </label>
+    </p>
+	<p><?php _e('When there are no posts, what should be shown?',CRP_LOCAL_NAME); ?><br />
+		<label>
+		<input type="radio" name="blank_output" value="blank" id="blank_output_0" <?php if ($crp_settings['blank_output']) echo 'checked="checked"' ?> />
+		<?php _e('Blank Output',CRP_LOCAL_NAME); ?></label>
+		<br />
+		<label>
+		<input type="radio" name="blank_output" value="noposts" id="blank_output_1" <?php if (!$crp_settings['blank_output']) echo 'checked="checked"' ?> />
+		<?php _e('Display "No Related Posts"',CRP_LOCAL_NAME); ?></label>
+		<br />
+	</p>
+	<h4><?php _e('Customize the output:',CRP_LOCAL_NAME); ?></h4>
+	<p>
+      <label>
+      <?php _e('HTML to display before the list of posts: ',CRP_LOCAL_NAME); ?>
+      <input type="textbox" name="before_list" id="before_list" value="<?php echo attribute_escape(stripslashes($crp_settings[before_list])); ?>">
+      </label>
+	</p>
+	<p>
+      <label>
+      <?php _e('HTML to display before each list item: ',CRP_LOCAL_NAME); ?>
+      <input type="textbox" name="before_list_item" id="before_list_item" value="<?php echo attribute_escape(stripslashes($crp_settings[before_list_item])); ?>">
+      </label>
+	</p>
+	<p>
+      <label>
+      <?php _e('HTML to display after each list item: ',CRP_LOCAL_NAME); ?>
+      <input type="textbox" name="after_list_item" id="after_list_item" value="<?php echo attribute_escape(stripslashes($crp_settings[after_list_item])); ?>">
+      </label>
+	</p>
+	<p>
+      <label>
+      <?php _e('HTML to display after the list of posts: ',CRP_LOCAL_NAME); ?>
+      <input type="textbox" name="after_list" id="after_list" value="<?php echo attribute_escape(stripslashes($crp_settings[after_list])); ?>">
+      </label>
+	</p>
+	<h4><?php _e('Post thumbnail options:',CRP_LOCAL_NAME); ?></h4>
+	<p>
+		<label>
+		<input type="radio" name="post_thumb_op" value="inline" id="post_thumb_op_0" <?php if ($crp_settings['post_thumb_op']=='inline') echo 'checked="checked"' ?> />
+		<?php _e('Display thumbnails inline with posts',CRP_LOCAL_NAME); ?></label>
+		<br />
+		<label>
+		<input type="radio" name="post_thumb_op" value="thumbs_only" id="post_thumb_op_1" <?php if ($crp_settings['post_thumb_op']=='thumbs_only') echo 'checked="checked"' ?> />
+		<?php _e('Display only thumbnails, no text',CRP_LOCAL_NAME); ?></label>
+		<br />
+		<label>
+		<input type="radio" name="post_thumb_op" value="text_only" id="post_thumb_op_2" <?php if ($crp_settings['post_thumb_op']=='text_only') echo 'checked="checked"' ?> />
+		<?php _e('Do not display thumbnails, only text.',CRP_LOCAL_NAME); ?></label>
+		<br />
+	</p>
+    <p>
+      <label>
+      <?php _e('Post thumbnail meta field (the meta should point contain the image source): ',CRP_LOCAL_NAME); ?>
+      <input type="textbox" name="thumb_meta" id="thumb_meta" value="<?php echo attribute_escape(stripslashes($crp_settings[thumb_meta])); ?>">
+      </label>
+    </p>
+    <p><strong><?php _e('Thumbnail dimensions:',CRP_LOCAL_NAME); ?></strong><br />
+      <label>
+      <?php _e('Max width: ',CRP_LOCAL_NAME); ?>
+      <input type="textbox" name="thumb_width" id="thumb_width" value="<?php echo attribute_escape(stripslashes($crp_settings[thumb_width])); ?>" style="width:30px">px
+      </label>
+	  <br />
+      <label>
+      <?php _e('Max height: ',CRP_LOCAL_NAME); ?>
+      <input type="textbox" name="thumb_height" id="thumb_height" value="<?php echo attribute_escape(stripslashes($crp_settings[thumb_height])); ?>" style="width:30px">px
+      </label>
+    </p>
+	<p><?php _e('The plugin will first check if the post contains a thumbnail. If it doesn\'t then it will check the meta field. If this is not available, then it will show the default image as specified below:',CRP_LOCAL_NAME); ?>
+	<input type="textbox" name="thumb_default" id="thumb_default" value="<?php echo attribute_escape(stripslashes($crp_settings[thumb_default])); ?>" style="width:500px">
+	</p>
+    <p>
+      <input type="submit" name="crp_save" id="crp_save" value="Save Options" style="border:#0C0 1px solid" />
+      <input name="crp_default" type="submit" id="crp_default" value="Default Options" style="border:#F00 1px solid" onclick="if (!confirm('<?php _e('Do you want to set options to Default?',CRP_LOCAL_NAME); ?>')) return false;" />
+      <input name="crp_recreate" type="submit" id="crp_recreate" value="Recreate index" style="border:#00c 1px solid" onclick="if (!confirm('<?php _e('Are you sure you want to recreate the index?',CRP_LOCAL_NAME); ?>')) return false;" />
+    </p>
+    </fieldset>
+  </form>
+</div>
+<?php
+
+}
+
+
+function crp_adminmenu() {
+	if (function_exists('current_user_can')) {
+		// In WordPress 2.x
+		if (current_user_can('manage_options')) {
+			$crp_is_admin = true;
+		}
+	} else {
+		// In WordPress 1.x
+		global $user_ID;
+		if (user_can_edit_user($user_ID, 0)) {
+			$crp_is_admin = true;
+		}
+	}
+
+	if ((function_exists('add_options_page'))&&($crp_is_admin)) {
+		$plugin_page = add_options_page(__("Related Posts", CRP_LOCAL_NAME), __("Related Posts", CRP_LOCAL_NAME), 9, 'crp_options', 'crp_options');
+		add_action( 'admin_head-'. $plugin_page, 'crp_adminhead' );
+	}
+	
+}
+add_action('admin_menu', 'crp_adminmenu');
+
+function crp_adminhead() {
+	global $crp_url;
+
+?>
+<link rel="stylesheet" type="text/css" href="<?php echo $crp_url ?>/wick/wick.css" />
+<script type="text/javascript" language="JavaScript">
+function checkForm() {
+answer = true;
+if (siw && siw.selectingSomething)
+	answer = false;
+return answer;
+}//
+</script>
+<script type="text/javascript" src="<?php echo $crp_url ?>/wick/sample_data.js.php"></script>
+<script type="text/javascript" src="<?php echo $crp_url ?>/wick/wick.js"></script>
+<?php }
+
+?>
---
+++ contextual-related-posts.php
@@ -0,0 +1,230 @@
+<?php
+/*
+Plugin Name: Contextual Related Posts
+Version:     1.5
+Plugin URI:  http://ajaydsouza.com/wordpress/plugins/contextual-related-posts/
+Description: Show user defined number of contextually related posts. Based on the plugin by <a href="http://weblogtoolscollection.com">Mark Ghosh</a>.  <a href="options-general.php?page=crp_options">Configure...</a>
+Author:      Ajay D'Souza
+Author URI:  http://ajaydsouza.com/
+*/
+
+if (!defined('ABSPATH')) die("Aren't you supposed to come here via WP-Admin?");
+
+define('ALD_crp_DIR', dirname(__FILE__));
+define('CRP_LOCAL_NAME', 'crp');
+
+function ald_crp_init() {
+	//* Begin Localization Code */
+	$crp_localizationName = CRP_LOCAL_NAME;
+	$crp_comments_locale = get_locale();
+	$crp_comments_mofile = ALD_crp_DIR . "/languages/" . $crp_localizationName . "-". $crp_comments_locale.".mo";
+	load_textdomain($crp_localizationName, $crp_comments_mofile);
+	//* End Localization Code */
+}
+add_action('init', 'ald_crp_init');
+
+
+/*********************************************************************
+*				Main Function (Do not edit)							*
+********************************************************************/
+function ald_crp() {
+	global $wpdb, $post, $single;
+
+	$crp_settings = crp_read_options();
+	$limit = (stripslashes($crp_settings['limit']));
+	$exclude_categories = explode(',',$crp_settings['exclude_categories']);
+	
+	// Make sure the post is not from the future
+	$time_difference = get_settings('gmt_offset');
+	$now = gmdate("Y-m-d H:i:s",(time()+($time_difference*3600)));
+
+	if($crp_settings['match_content']) {
+		$stuff = addslashes($post->post_title. ' ' . $post->post_content);
+	}
+	else {
+		$stuff = addslashes($post->post_title);
+	}
+	
+	
+	if ((is_int($post->ID))&&($stuff != '')) {
+		$sql = "SELECT DISTINCT ID,post_title,post_date,"
+		. "MATCH(post_title,post_content) AGAINST ('".$stuff."') AS score "
+		. "FROM ".$wpdb->posts." WHERE "
+		. "MATCH (post_title,post_content) AGAINST ('".$stuff."') "
+		. "AND post_date <= '".$now."' "
+		. "AND post_status = 'publish' "
+		. "AND id != ".$post->ID." ";
+		if ($crp_settings['exclude_pages']) $sql .= "AND post_type = 'post' ";
+		$sql .= "ORDER BY score DESC ";
+		
+		$search_counter = 0;
+		$searches = $wpdb->get_results($sql);
+	} else {
+		$searches = false;
+	}
+	
+	$output = '<div id="crp_related">';
+	
+	if($searches){
+		$output .= (stripslashes($crp_settings[title]));
+		$output .= $crp_settings['before_list'];
+		foreach($searches as $search) {
+			$categorys = get_the_category($search->ID);	//Fetch categories of the plugin
+			$p_in_c = false;	// Variable to check if post exists in a particular category
+			$title = trim(stripslashes($search->post_title));
+			foreach ($categorys as $cat) {	// Loop to check if post exists in excluded category
+				$p_in_c = (in_array($cat->cat_ID, $exclude_categories)) ? true : false;
+				if ($p_in_c) break;	// End loop if post found in category
+			}
+
+			if (!$p_in_c) {
+				$output .= $crp_settings['before_list_item'];
+				if (($crp_settings['post_thumb_op']=='inline')||($crp_settings['post_thumb_op']=='thumbs_only')) {
+					$output .= '<a href="'.get_permalink($search->ID).'" rel="bookmark">';
+					if ((function_exists('has_post_thumbnail')) && (has_post_thumbnail($search->ID))) {
+						$output .= get_the_post_thumbnail( $search->ID, array($crp_settings[thumb_width],$crp_settings[thumb_height]), array('title' => $title,'alt' => $title));
+					} else {
+						$postimage = get_post_meta($search->ID, 'post-image', true);
+						if ($postimage) {
+							$output .= '<img src="'.$postimage.'" alt="'.$title.'" title="'.$title.'" width="'.$crp_settings[thumb_width].'" height="'.$crp_settings[thumb_height].'" />';
+						} else {
+							$output .= '<img src="'.$crp_settings[thumb_default].'" alt="'.$title.'" title="'.$title.'" width="'.$crp_settings[thumb_width].'" height="'.$crp_settings[thumb_height].'" />';
+						}
+					}
+					$output .= '</a> ';
+				}
+				if (($crp_settings['post_thumb_op']=='inline')||($crp_settings['post_thumb_op']=='text_only')) {
+					$output .= '<a href="'.get_permalink($search->ID).'" rel="bookmark">'.$title.'</a>';
+				}
+				$output .= $crp_settings['after_list_item'];
+				$search_counter++; 
+			}
+			if ($search_counter == $limit) break;	// End loop when related posts limit is reached
+		} //end of foreach loop
+		$output .= $crp_settings['after_list'];
+	}else{
+		$output = '<div id="crp_related">';
+		$output .= ($crp_settings['blank_output']) ? ' ' : '<p>'.__('No related posts found',CRP_LOCAL_NAME).'</p>'; 
+	}
+	if ((strpos($output, '<li>')) === false) {
+		$output = '<div id="crp_related">';
+		$output .= ($crp_settings['blank_output']) ? ' ' : '<p>'.__('No related posts found',CRP_LOCAL_NAME).'</p>'; 
+	}
+	$output .= '</div>';
+	
+	return $output;
+}
+
+function ald_crp_content($content) {
+	
+	global $single;
+	$crp_settings = crp_read_options();
+	$output = ald_crp();
+	
+    if((is_single())&&($crp_settings['add_to_content'])) {
+        return $content.$output;
+    } elseif((is_page())&&($crp_settings['add_to_page'])) {
+        return $content.$output;
+	} elseif((is_feed())&&($crp_settings['add_to_feed'])) {
+        return $content.$output;
+    } else {
+        return $content;
+    }
+}
+add_filter('the_content', 'ald_crp_content');
+
+function echo_ald_crp() {
+	$output = ald_crp();
+	echo $output;
+}
+
+// Default Options
+function crp_default_options() {
+	$title = __('<h3>Related Posts:</h3>',CRP_LOCAL_NAME);
+	$thumb_default = get_bloginfo('wpurl').'/wp-content/plugins/contextual-related-posts/default.png';
+
+	$crp_settings = 	Array (
+						title => $title,			// Add before the content
+						add_to_content => true,		// Add related posts to content (only on single posts)
+						add_to_page => false,		// Add related posts to content (only on single pages)
+						add_to_feed => true,		// Add related posts to feed
+						limit => '5',				// How many posts to display?
+						match_content => true,		// Match against post content as well as title
+						exclude_pages => true,		// Exclude Pages
+						blank_output => true,		// Blank output?
+						exclude_categories => '',	// Exclude these categories
+						exclude_cat_slugs => '',	// Exclude these categories (slugs)
+						before_list => '<ul>',	// Before the entire list
+						after_list => '</ul>',	// After the entire list
+						before_list_item => '<li>',	// Before each list item
+						after_list_item => '</li>',	// After each list item
+						post_thumb_op => 'text_only',	// Display only text in posts
+						thumb_height => '100',	// Height of thumbnails
+						thumb_width => '100',	// Width of thumbnails
+						thumb_meta => 'post-image',	// Meta field that is used to store the location of default thumbnail image
+						thumb_default => $thumb_default,	// Default thumbnail image
+						);
+	return $crp_settings;
+}
+
+// Function to read options from the database
+function crp_read_options() 
+{
+	$crp_settings_changed = false;
+	
+	$defaults = crp_default_options();
+	
+	$crp_settings = array_map('stripslashes',(array)get_option('ald_crp_settings'));
+	unset($crp_settings[0]); // produced by the (array) casting when there's nothing in the DB
+	
+	foreach ($defaults as $k=>$v) {
+		if (!isset($crp_settings[$k]))
+			$crp_settings[$k] = $v;
+		$crp_settings_changed = true;	
+	}
+	if ($crp_settings_changed == true)
+		update_option('ald_crp_settings', $crp_settings);
+	
+	return $crp_settings;
+
+}
+
+// Create full text index
+function ald_crp_activate() {
+	global $wpdb;
+
+    $wpdb->hide_errors();
+    $wpdb->query('ALTER TABLE '.$wpdb->posts.' ENGINE = MYISAM;');
+    $wpdb->query('ALTER TABLE '.$wpdb->posts.' ADD FULLTEXT crp_related (post_title, post_content);');
+    $wpdb->query('ALTER TABLE '.$wpdb->posts.' ADD FULLTEXT crp_related_title (post_title);');
+    $wpdb->query('ALTER TABLE '.$wpdb->posts.' ADD FULLTEXT crp_related_content (post_content);');
+    $wpdb->show_errors();
+}
+if (function_exists('register_activation_hook')) {
+	register_activation_hook(__FILE__,'ald_crp_activate');
+}
+
+// This function adds an Options page in WP Admin
+if (is_admin() || strstr($_SERVER['PHP_SELF'], 'wp-admin/')) {
+	require_once(ALD_crp_DIR . "/admin.inc.php");
+}
+
+// Add meta links
+function crp_plugin_actions( $links, $file ) {
+	$plugin = plugin_basename(__FILE__);
+ 
+	// create link
+	if ($file == $plugin) {
+		$links[] = '<a href="' . admin_url( 'options-general.php?page=crp_options' ) . '">' . __('Settings', crp_LOCAL_NAME ) . '</a>';
+		$links[] = '<a href="http://ajaydsouza.org">' . __('Support', CRP_LOCAL_NAME ) . '</a>';
+		$links[] = '<a href="http://ajaydsouza.com/donate/">' . __('Donate', CRP_LOCAL_NAME ) . '</a>';
+	}
+	return $links;
+}
+global $wp_version;
+if ( version_compare( $wp_version, '2.8alpha', '>' ) )
+	add_filter( 'plugin_row_meta', 'crp_plugin_actions', 10, 2 ); // only 2.8 and higher
+else add_filter( 'plugin_action_links', 'crp_plugin_actions', 10, 2 );
+
+
+?>
